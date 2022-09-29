e9f9036efa48b2197cca2a8b21210895dd73d97b
Mittal Patel <mittal.patel@1Rintern.local>
2022-09-29 17:27:47 +0530

    subject

--- Angular-JS/src/app/home/home.component.ts
+++ Angular-JS/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AsyncSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public subject = new AsyncSubject();
+  public observer1:any;
+  public observer2:any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void{
+    this.observer1 = this.subject.subscribe({
+      next:(msg)=>{
+        console.log("1stNext "+msg);
+      },
+      error:(err)=>{
+        console.log(err);
+      },
+      complete:()=>{
+        console.log("Completed")
+      }
+    })
+    this.subject.next(1)
+    this.subject.next(2)
+    this.subject.complete()
+    this.subject.next(3)
+    
+    this.observer2 = this.subject.subscribe({
+      next:(msg)=>{
+        console.log("2ndNext  "+msg);
+      },
+      error:(err)=>{
+        console.log(err);
+      },
+      complete:()=>{
+        console.log("Completed")
+      }
+    })
+    this.subject.next(4);
+  }
 }
